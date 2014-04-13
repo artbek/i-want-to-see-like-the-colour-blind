23fffffce4f2a442b70a756cf17d0daa626cde32
artbek <bart@artbek.co.uk>
2014-04-13 14:59:44 +0100

    Nicer design.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # I want to see like the colour blind
 
-Right-click on a page and select 'Colour Blind Experience' from the context menu.
+Right-click on a page and select 'Experience Colour Blindness' from the context menu.
 
 ## Options:
 - SVG data file (path to a .svg file on http://altreus.github.io domain)
--- js/content_script.js
+++ js/content_script.js
@@ -21,18 +21,38 @@ function iWantToSeeLikeTheColourBlind(svg_data) {
 
 	var popup_css = '' +
 		' .colour_blind_popup_button { ' +
-		'    margin: 3px; ' +
-		'    padding: 5px; ' +
+		'   border: 0px; ' +
+		'   border-bottom: 1px solid #9999aa; ' +
+		'   color: #5d5d6f; ' +
+		'   cursor: pointer; ' +
+		'   font-family: monospace; ' +
+		'   font-size: 14px; ' +
+		'   line-height: 14px; ' +
+		'   margin: 16px 0 0 0; ' +
+		'   padding: 0 0 5px 0; ' +
+		'   text-align: left; ' +
+		'   width: 100%; ' +
+		' } ' +
+
+		' .colour_blind_popup_button.active { ' +
+		'   border-bottom: 1px solid #333344; ' +
+		'   color: #222233; ' +
+		'   font-weight: bold; ' +
 		' } ' +
 
 		' #color_blind_popup { ' +
-		'   position: fixed; ' +
-		'   top: 20px; ' +
-		'   right: 20px; ' +
-		'   z-index: 99999; ' +
-		'   padding: 10px; ' +
 		'   background: #f9f9f9; ' +
-		'   border: 1px solid #bbbbbb; ' +
+		'   background-image: url("' + chrome.extension.getURL("img/bg.jpg") + '"); ' +
+		'   background-position: center center; ' +
+		'   border: 1px solid #eeeeee; ' +
+		'   border-radius: 4px; ' +
+		'   box-shadow: -4px 4px 25px #333333; ' +
+		'   opacity: 0.95; ' +
+		'   padding: 15px 35px 35px 30px; ' +
+		'   position: fixed; ' +
+		'   right: 30px; ' +
+		'   top: 30px; ' +
+		'   z-index: 99999; ' +
 		' } ';
 
 	if (! document.getElementById('colourblind-popup-styling')) {
@@ -62,9 +82,13 @@ function iWantToSeeLikeTheColourBlind(svg_data) {
 		var popup_html = '';
 
 		for (var i in colour_perception_modes) {
-			popup_html += '<button class="colour_blind_popup_button" id="mode_' + i + '">';
+			if (i == 0) {
+				popup_html += '<div class="colour_blind_popup_button active" id="mode_' + i + '">';
+			} else {
+				popup_html += '<div class="colour_blind_popup_button" id="mode_' + i + '">';
+			}
 			popup_html += colour_perception_modes[i];
-			popup_html += '</button><br />';
+			popup_html += '</div>';
 		}
 
 		var prompt_div = document.createElement('div');
@@ -94,8 +118,9 @@ function iWantToSeeLikeTheColourBlind(svg_data) {
 		'   filter:!!; ' +
 		' } ';
 
-	prompt_div.addEventListener("click", function(e) {
+	document.getElementById('color_blind_popup').addEventListener("click", function(e) {
 		e.stopPropagation();
+
 		var mode_name = e.srcElement.innerText;
 		var s = document.getElementById('colourblind-styling');
 		if (document.getElementById(mode_name)) {
@@ -104,6 +129,12 @@ function iWantToSeeLikeTheColourBlind(svg_data) {
 		} else {
 			s.innerHTML = normal_mode_css;
 		}
+
+		var buttons = document.getElementsByClassName('colour_blind_popup_button');
+		for (var i = 0; i < buttons.length; i++) {
+			buttons[i].classList.remove("active");
+		}
+		e.srcElement.classList.add("active");
 	});
 
 
